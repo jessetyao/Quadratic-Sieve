0f7ce73db38fa409962ea3ef97155790a93bd5fb
jesse11883 <jessetyao@berkeley.edu>
2023-10-14 11:32:48 -0700

    start

---
+++ basicRSA.py
@@ -0,0 +1,32 @@
+import math
+from Crypto.Util import number
+
+def generate_prime(n):
+    return number.getPrime(n)
+
+def generate_e(H):
+    e = 3
+    while(e<H):
+        if (math.gcd(e, H) == 1):
+            return e
+        else:
+            e += 2
+
+p = generate_prime(52)
+q = generate_prime(52)
+N = p * q
+H = (p-1) * (q-1)
+e = generate_e(H)
+public_key = (N,e)
+d = pow(e, -1, H)
+msg = generate_prime(52)
+print(msg)
+encrypt = pow(msg, e, N)
+print(encrypt)
+decrypt = pow(encrypt, d, N)
+print(decrypt)
+
+# def break_Rsa (public_key, encrypt):
+#     n, e = public_key
+
+
